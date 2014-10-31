6ac488f2f019df77958d4852922f9209c8b891bc
Antoine Jackson <a.j.william26@gmail.com>
2014-10-31 12:10:28 +0100

    Initial commit

---
+++ OAuthioSwiftExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OAuthioSwiftExample
+//
+//  Created by Antoine Jackson on 31/10/14.
+//  Copyright (c) 2014 OAuth.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
